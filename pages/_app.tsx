58bca5e6a84807d6c5682138206562e4e5f3525f
cofeek-codes <kormyshev11@mail.ru>
2022-11-02 00:24:00 +0500

    analytics added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import '../styles/globals.scss'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
-
+import { Analytics } from '@vercel/analytics/react'
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<ChakraProvider>
 			<Component {...pageProps} />
+			<Analytics />
 		</ChakraProvider>
 	)
 }
