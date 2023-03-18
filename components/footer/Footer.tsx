caa67aca8fdd0b4519207e7081b74b94a0a7d3bb
cofeek-codes <kormyshev11@mail.ru>
2023-03-18 23:13:44 +0500

    font fixed

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,7 +1,11 @@
+import styles from './footer.module.scss'
 const Footer = () => {
   return (
   <footer>
+      <div className={styles.copyright}>
+
       copyright &copy; cofeek-codes, {(new Date().getFullYear())}
+      </div>
     </footer>
   )
 }
