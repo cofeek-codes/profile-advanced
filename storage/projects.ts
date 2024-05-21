41af9ac8e9a6ba96cd02e6d4cc0f175f6a815022
cofeek-codes <kormyshev11@mail.ru>
2024-05-21 15:30:52 +0500

    new project added

--- storage/projects.ts
+++ storage/projects.ts
@@ -1,22 +1,29 @@
 import pcImage from '../assets/pc-course-image.png'
 import portfolioImage from '../assets/portfolio.png'
+import japanImage from '../assets/japan.png'
+
 export type Project = {
-	name: string
-	image: any
-	href: string
+   name: string
+   image: string
+   href: string
 }
 
 const Projects: Project[] = [
-	{
-		name: 'PC Course',
-		image: pcImage,
-		href: 'https://pc-course.vercel.app/',
-	},
-	{
-		name: 'Portfolio Example',
-		image: portfolioImage,
-		href: 'https://john-virid.vercel.app/',
-	},
+   {
+      name: 'PC Course',
+      image: pcImage,
+      href: 'https://pc-course.vercel.app/',
+   },
+   {
+      name: 'Portfolio Example',
+      image: portfolioImage,
+      href: 'https://john-virid.vercel.app/',
+   },
+   {
+      name: 'Travel Tour Landing',
+      image: japanImage,
+      href: 'https://jap-93e048gj1-cofeekcodes-projects.vercel.app/',
+   },
 ]
 
 export default Projects
