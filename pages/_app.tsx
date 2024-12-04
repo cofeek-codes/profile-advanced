371f983c9e5e1cf64fff430b69acc1011f86e00d
cofeek-codes <kormyshev11@mail.ru>
2024-12-04 19:14:30 +0500

    web analytic dependencies update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,14 @@ import '../styles/globals.scss'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
+
 function MyApp({ Component, pageProps }: AppProps) {
+	// @FIXME: workaround of new wierd nextjs error
+	const CustomComoponent = Component as any
+
 	return (
 		<ChakraProvider>
-			<Component {...pageProps} />
+			<CustomComoponent {...pageProps} />
 			<Analytics />
 		</ChakraProvider>
 	)
